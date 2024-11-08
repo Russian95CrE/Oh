4c1d0561d91133fd4a51ec7bd7407ddcc81fdca6
Russian95CrE <russian95@enhance11.live>
2024-11-08 16:01:52 -0300

    input on the impossible

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
 function playGame() {
     window.location.href = 'Level01/index.html';
 }
+
+function impossibleChallenge() {
+    const youCry = prompt("Are you sure? You will cry after starting, type 'Yes, i will cry like a baby'")
+
+    if (youCry === "Yes, i will cry like a baby") {
+        window.location.href = 'impossible_challenge/index.html';
+    } else {
+        alert("Loser.")
+    }
+}
